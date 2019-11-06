21078800a9a38905b2c597ee08d56a4f7c6a484e
Philipp Thomas <philipp.thomas@diva-e.com>
2019-11-06 20:48:15 +0100

    Add JUnit5 tests with shared network

---
+++ src/test/java/de/phitho/testcontainers/examples/junit4/GenericContainerWithSharedNetworkTest.java
@@ -0,0 +1,35 @@
+package de.phitho.testcontainers.examples.junit4;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+
+import java.io.File;
+
+public class GenericContainerWithSharedNetworkTest {
+
+    @Rule
+    public final BrowserWebDriverContainer chromeContainer = new BrowserWebDriverContainer<>()
+                    .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("build"))
+                    .withCapabilities(new ChromeOptions())
+                    .withNetwork(Network.SHARED);
+
+    @ClassRule
+    public static final GenericContainer appToTest = new GenericContainer("solr:latest")
+            .withNetwork(Network.SHARED)
+            .withNetworkAliases("solr");
+
+    @Test
+    public void name() {
+        final WebDriver webDriver = chromeContainer.getWebDriver();
+
+        // use the alias 'solr' as defined for the shared docker network
+        // using Network.SHARED both containers communicate within the network
+        webDriver.get("http://solr:8983/solr");
+    }
+}
